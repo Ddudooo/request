34076c2ece9ac9721b921b92bc4ca07bb15c2c39
Jungdoo <ddudoo95@gmail.com>
2019-12-06 17:07:35 +0900

    Update action

--- README.md
+++ README.md
@@ -1,5 +1,15 @@
 # REQUEST GITHUB ACTION
 
 ## USAGE
-
+```
+- name: Webhook
+  uses: ddudooo/request@master
+  with:
+    METHOD: POST
+    Headers: '{"Content-type":"application/json"}'
+    URL: localhost
+    data: '{
+      "EXAMPLE": "THIS IS EXAMPLE DATA",      
+    }'
+```
 ## OTHER
--- src/main.ts
+++ src/main.ts
@@ -1,33 +1,27 @@
-import core from "@actions/core";
-import github from "@actions/github";
-import request from "request-promise-native";
+const core = require("@actions/core");
+const request = require("request-promise-native");
 
 async function run() {
     try {
-        // const myInput = core.getInput("myInput");
-        // core.debug(`Hello ${myInput} from inside a container`);
         const METHOD = core.getInput("Method", {required: false});
-        const HEADERS = core.getInput("Content-type", {required: false});
-        const data = core.getInput("data", {required: true});
+        const HEADERS = core.getInput("Headers", {required: false});
+        const DATA = core.getInput("data", {required: true});
         const URL = core.getInput("URL", {required: true});
-        // // Get github context data
-        // const context = github.context;
-        // console.log(
-        //     `We can even get context data, like the repo: ${context.repo.repo}`
-        // );
         const OPTIONS = {
             url: URL,
             method: METHOD,
-            headers: HEADERS,
-            body: data
+            headers: JSON.parse(HEADERS),
+            body: DATA
         };
-        request(OPTIONS)
-            .then(res => console.log(`RESPONSE ${res.body}`))
+        console.log(OPTIONS);
+        await request(OPTIONS)
+            .then(res => console.log(`RESPONSE ${res}`))
             .catch(err => {
                 throw err;
             });
     } catch (error) {
-        core.setFailed(error.message);
+        console.log(error);
+        core.setFailed("FAIL TO REQUEST");
     }
 }
 
